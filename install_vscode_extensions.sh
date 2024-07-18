d7023792a07edcc10b785eb163a05998cb24d328
Juan A. Hernandez <91210620+runtimeacademy@users.noreply.github.com>
2024-07-17 21:32:52 -0500

    Update install_vscode_extensions.sh

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -4,11 +4,9 @@
 #copy file https://github.com/runtimeacademy/kasm_workspace.git/install_vscode_extensions.sh 
 #chmod +x install_vscode_extensions.sh
 
-# Extension identifier
+#Extension identifier
 WASP="wasp-lang.wasp"
 PRISMA="prisma.prisma"
 
-
 code --install-extension $WASP
-
 code --install-extension $PRISMA
