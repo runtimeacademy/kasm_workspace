9ddd2e026f7a0a3ecc1a9729970419ed183b9676
Juan A. Hernandez <91210620+runtimeacademy@users.noreply.github.com>
2024-07-17 21:28:50 -0500

    Update install_vscode_extensions.sh

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -5,22 +5,10 @@
 #chmod +x install_vscode_extensions.sh
 
 # Extension identifier
-EXTENSION="wasp-lang.wasp-0.5.3"
+WASP="wasp-lang.wasp"
+PRISMA="prisma.prisma"
 
-# Check if 'code' command is available
-if ! command -v code &> /dev/null
-then
-    echo "'code' command not found. Please ensure Visual Studio Code is installed and added to your PATH."
-    exit 1
-fi
 
-# Install the extension
-echo "Installing the wasp-lang extension for VSCode..."
-code --install-extension $EXTENSION
+code --install-extension $WASP
 
-if [ $? -eq 0 ]; then
-    echo "wasp-lang extension installed successfully."
-else
-    echo "Failed to install the wasp-lang extension."
-    exit 1
-fi
+code --install-extension $PRISMA
